e17cec2553bff38cff16bfebd02b9e9effb42d75
Juan Cuenca <112384388+JuanC3719@users.noreply.github.com>
2023-05-15 23:48:21 -0700

    Create BlogType.cs

---
+++ dotNet/BlogType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrately.Models.Domain
+{
+    public class BlogType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
