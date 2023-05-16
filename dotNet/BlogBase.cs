0ff63c3971cdb9bd0fb5825af082307dda3a5043
Juan Cuenca <112384388+JuanC3719@users.noreply.github.com>
2023-05-15 23:49:55 -0700

    Update BlogBase.cs

--- dotNet/BlogBase.cs
+++ dotNet/BlogBase.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Migrately.Models.Requests
+namespace Migrately.Models.Domain
 {
-    public class BlogIsDeletedUpdateRequest : BlogAddRequest , IModelIdentifier 
+    public class BlogBase
     {
         public int Id { get; set; }
-        
+        public LookUp BlogType { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public Uri ImageUrl { get; set; }
+   
     }
 }
