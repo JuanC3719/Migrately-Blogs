d0a613605940ab4c29d0660998bb6daed3e15822
Juan Cuenca <112384388+JuanC3719@users.noreply.github.com>
2023-08-22 15:58:54 -0700

    Update IBlogService.cs

--- dotNet/IBlogService.cs
+++ dotNet/IBlogService.cs
@@ -1,17 +1,12 @@
-using Migrately.Models;
 using Migrately.Models.Domain;
 using Migrately.Models.Requests;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Migrately.Services.Interfaces
 {
     public interface IBlogService
     {
-        public Blog GetBlogById(int id);
+        Blog GetBlogById(int id);
         int AddBlog(BlogAddRequest model);
         Paged<Blog> GetAllBlogsByPage(int pageIndex, int pageSize);
         void UpdateBlog(BlogUpdateRequest model);
@@ -19,6 +14,6 @@ namespace Migrately.Services.Interfaces
         Paged<Blog> GetByBlogType(int pageIndex, int pageSize, int blogTypeId);
         Paged<Blog> GetBlogByAuthorId(int authorId, int pageIndex, int pageSize);
         Paged<Blog> SearchBlogs(int pageIndex, int pageSize, string query);
-        public List<BlogType> GetAllBlogTypes();
+        List<BlogType> GetAllBlogTypes();
     }
 }
